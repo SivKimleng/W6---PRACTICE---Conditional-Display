ac315a1d06f76893a3b72f6d83de5e468b156ea9
SivKimleng <singsingsiv@gmail.com>
2026-03-01 12:57:38 +0700

    finish

--- src/components/Scores.jsx
+++ src/components/Scores.jsx
@@ -8,34 +8,83 @@ function getScoreStyle(score) {
     }
     return "";
   }
+function calculateAvg(students) {
+    let sum = 0;
+    students.forEach((student) => {
+      sum += student.score;
+    });
+    return (sum / students.length).toFixed(0);
+  }
+
+function findMin(students) {
+    let min = students[0].score;
+    students.forEach((student) => {
+      if (student.score < min) {
+        min = student.score;
+      }
+    });
+    return min;
+  }
+
+function findMax(students) {
+    let max = students[0].score;
+    students.forEach((student) => {
+      if (student.score > max) {
+        max = student.score;
+      }
+    });
+    return max;
+  }
 
   return (
-    <>
-      <div className="scores">
-        <h1>{props.courseName}</h1>
+    <div className="scores">
+      <h1>{props.courseName}</h1>
 
-        <table>
-          <thead>
-            <tr>
-              <th>First name</th>
-              <th>Last name</th>
-              <th>Score</th>
+      <table>
+        <thead>
+          <tr>
+            <th>First name</th>
+            <th>Last name</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {props.courseResults.map((student, index) => (
+            <tr key={index}>
+              <td>{student.firstName.trim()}</td>
+              <td>{student.lastName}</td>
+              <td className={getScoreStyle(student.score)}>
+                {student.score}
+              </td>
             </tr>
-          </thead>
+          ))}
+        </tbody>
+      </table>
 
-          <tbody>
-            {props.courseResults.map((student, index) => (
-              <tr key={index}>
-                <td>{student.firstName.trim()}</td>
-                <td>{student.lastName}</td>
-                <td className={getScoreStyle(student.score)}>
-                  {student.score}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+      <div className="statistics">
+        <div className="stat-item">
+          <span className="stat-label">AVERAGE</span>
+          <span className="stat-value">
+            {calculateAvg(props.courseResults)}
+          </span>
+        </div>
+
+        <div className="stat-item">
+          <span className="stat-label">MIN</span>
+          <span className="stat-value">
+            {findMin(props.courseResults)}
+          </span>
+        </div>
+
+        <div className="stat-item">
+          <span className="stat-label">MAX</span>
+          <span className="stat-value">
+            {findMax(props.courseResults)}
+          </span>
+        </div>
       </div>
-    </>
+
+    </div>
   );
 }
